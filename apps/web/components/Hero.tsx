49096d57231a8cd2db4c0ca3faf7ef13222ca756
kimhayeon <hayeon.kim@vivity.ai>
2025-04-02 00:43:16 +0900

    [add] 이미지 첨부

--- apps/web/components/Hero.tsx
+++ apps/web/components/Hero.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image'
 export default function Hero() {
   return (
     <section id="hero" className="flex flex-col items-center bg-gray-100 py-16 pt-20 text-center">
-      <Image src={'../public/avatar.png'} width={250} height={250} alt="avatar" />
+      <Image src={'/avatar.png'} width={250} height={250} alt="avatar" />
       <h1 className="mt-4 text-3xl font-bold">Hi, I'm Hayeon.</h1>
       <p className="text-gray-600">FrontEnd Developer</p>
       <a

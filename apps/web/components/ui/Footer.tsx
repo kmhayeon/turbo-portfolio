b2a1fb68518fb928e705e6e76f4911e4e3a0f4ff
kimhayeon <hayeon.kim@vivity.ai>
2025-04-02 01:19:16 +0900

    [add] footer 수정

--- apps/web/components/ui/Footer.tsx
+++ apps/web/components/ui/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
 
   return (
     <footer className="border-t px-4 py-8">
-      <div className="flex w-full justify-center md:justify-end">
+      <div className="flex w-full justify-end">
         <p className="text-sm text-gray-500">
           Copyright {currentYear}. Hayeon. All rights reserved.
         </p>

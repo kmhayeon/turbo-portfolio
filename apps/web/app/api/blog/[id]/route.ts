2735d0ce36169405e62723b911332b2572d72c60
kimhayeon <hayeon.kim@vivity.ai>
2025-04-02 17:35:34 +0900

    [add] 빌드에러2

--- apps/web/app/api/blog/[id]/route.ts
+++ apps/web/app/api/blog/[id]/route.ts
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { prisma } from '../../../../lib/prisma'
+
 export async function GET(req: NextRequest, context: any) {
   const { id } = context.params
 

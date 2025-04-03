2e46d4c3bb797b7adf4c68e0dd3e64b8aa78eeb8
kimhayeon <hayeon.kim@vivity.ai>
2025-04-03 16:41:04 +0900

    [add] 수정버튼 스타일 변경

--- apps/web/app/blog/[id]/page.tsx
+++ apps/web/app/blog/[id]/page.tsx
@@ -147,7 +147,7 @@ export default function BlogDetailPage(props: any) {
             <button
               title="수정"
               onClick={handleEdit}
-              className="px-4 py-2 text-sm text-gray-900 hover:text-gray-700"
+              className="rounded-[4px] border border-gray-300 px-2 py-2 text-sm text-gray-900 hover:bg-gray-100"
             >
               <Pencil size={18} />
             </button>

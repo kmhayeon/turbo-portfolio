146656d8b621130feac98affd87c07f05ced2aa6
hayeon kim <kmhayeon@naver.com>
2025-05-18 22:48:02 +0900

    [add] 메뉴 제거

--- apps/docs/components/Header.tsx
+++ apps/docs/components/Header.tsx
@@ -7,14 +7,14 @@ export function Header() {
     <header className="bg-background text-foreground border-b">
       <div className="mx-auto flex items-center px-6 py-6 pl-4 pr-4">
         <div className="text-white-500 text-lg font-bold">Binance Futures</div>
-        <nav className="flex gap-6 text-sm font-medium">
-          <Link
-            href="/"
-            className="text-foreground px-10 text-sm text-gray-500 no-underline hover:text-white"
-          >
-            수익률 비교
-          </Link>
-        </nav>
+        {/*<nav className="flex gap-6 text-sm font-medium">*/}
+        {/*  <Link*/}
+        {/*    href="/"*/}
+        {/*    className="text-foreground px-10 text-sm text-gray-500 no-underline hover:text-white"*/}
+        {/*  >*/}
+        {/*    수익률 비교*/}
+        {/*  </Link>*/}
+        {/*</nav>*/}
       </div>
     </header>
   )

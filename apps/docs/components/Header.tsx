dda640ac982578b406591ff9f4114528daaab82d
hayeonkim <kmhayeon@naver.com>
2025-05-19 13:39:26 +0900

    [add] 이미지 전체 수정

--- apps/docs/components/Header.tsx
+++ apps/docs/components/Header.tsx
@@ -4,7 +4,13 @@ export function Header() {
   return (
     <header className="bg-background text-foreground border-b">
       <div className="mx-auto flex items-center px-6 py-6">
-        <img src="/logo.png" alt="Binance Futures Logo" width={25} height={25} />
+        <img
+          src="/TURTURES_5.png"
+          alt="Binance Futures Logo"
+          width={25}
+          height={25}
+          className="rounded-[4px]"
+        />
         <div className="pl-2 text-xl font-bold text-white">TURTURES</div>
       </div>
     </header>

c5c48cb5bd392b95f52f3f204c16597144ae1c9b
hayeonkim <kmhayeon@naver.com>
2025-05-19 12:01:50 +0900

    [add] 불필요한 코드 제거

--- apps/docs/components/Header.tsx
+++ apps/docs/components/Header.tsx
@@ -1,21 +1,8 @@
 'use client'
 
-import Link from 'next/link'
-
 export function Header() {
   return (
     <header className="bg-background text-foreground border-b">
-      {/*<div className="mx-auto flex items-center px-6 py-6">*/}
-      {/*  <div className="text-white-500 text-lg font-bold">Binance Futures</div>*/}
-      {/*<nav className="flex gap-6 text-sm font-medium">*/}
-      {/*  <Link*/}
-      {/*    href="/"*/}
-      {/*    className="text-foreground px-10 text-sm text-gray-500 no-underline hover:text-white"*/}
-      {/*  >*/}
-      {/*    수익률 비교*/}
-      {/*  </Link>*/}
-      {/*</nav>*/}
-      {/*</div>*/}
       <div className="mx-auto flex items-center px-6 py-6">
         <img src="/logo.png" alt="Binance Futures Logo" width={25} height={25} />
         <div className="pl-2 text-xl font-bold text-white">Binance Futures</div>
